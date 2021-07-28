138871fbdd555b90bb0e6717d416e4e6f015d2db
Pawan Gupta <pawan.gupta@beyondkey.com>
2021-07-28 12:07:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './Home';
+import About from './About';
+import Service from './Service';
+import Contact from './Contact';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/service" component={Service}></Route>
+        <Route exact path="/about" component={About}></Route>
+        <Route exact path="/contact" component={Contact}></Route>
+        <Redirect to="/"></Redirect>
+      </Switch>
+      {/* <Footer /> */}
+    </>
   );
 }
 
